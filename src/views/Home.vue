af8e0d28ed1c0662582300d2beb8f1fb2b87818c
PeterH <pete08@gmail.com>
2020-08-03 19:40:40 -0500

    Initial commit for vue file and added Axios to navigate to backend app contianing backend data to display using front end JS -> Vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
 
+</style>
+
+
+
+<script>
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Vue.js! HEllo@"
+    };
+  },
+  created: function() {},
+  methods: {}
+};
 </script>
