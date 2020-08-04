04dce0ec8b57383f874337aad3399dd0333a4163
PeterH <pete08@gmail.com>
2020-08-03 21:36:52 -0500

    Add to home.vue in order to get user input and use input to send to mini_sample_capstone using axios then updating schema based on original user input then sending that addition to backend to the front end through Vue.js ... all using the axios library within vue package on JS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+
+    ---------------------
+    <br>
+    <button v-on:click="indexRecipes()">Get some data...maybe</button>
+    <br>
+    <button v-on:click="indexProducts()">Get some product data from the api</button>
+    <br>
+    <button v-on:click="createProduct()">Get it! Make IT!</button>
+    <br>
+    <p>name: <input type="text" v-model="name"></p>
+    <p>description: <input type="text" v-model="description"></p>
+    <p>price: <input type="text" v-model="price"></p>
+    <p>image url: <input type="text" v-model="image_url"></p>
+    <br>
+    <br>
+    <hr>
+    <div v-for="product in products">
+      <p> name: {{ product.name }} </p>
+      <p> description: {{ product.description }} </p>
+      <p> price: {{ product.price }} </p>
+      <p> img: {{ product.image_url }} </p>
+      <img v-bind:src="product.image_url">
+      <hr>
+    </div>
+    <br>
   </div>
 </template>
 
@@ -11,13 +36,51 @@
 
 
 <script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js! HEllo@"
+      message: "Welcome to Vue.js! HEllo@",
+      products: [],
     };
   },
-  created: function() {},
-  methods: {}
+
+  created: function() {
+    this.indexProducts();
+  },
+
+  methods: {
+
+    indexRecipes: function() {
+      console.log("HI THERE!! From console");
+    },
+    
+    indexProducts: function() {
+      console.log("You executed the indexProducts by either refreshign page or clicking  event listener button");
+      axios.get('api/products').then(response => {
+        console.log(response);
+        this.products = response.data;
+      });
+    },
+
+
+
+    createProduct: function() {
+      console.log("hi from createPRoduct method...");
+      
+      var params = {
+        name: this.name,
+        description: this.description,
+        price: this.price,
+        image_url: this.image_url
+      };
+      
+      axios.post('api/products', params).then(response => {
+        this.products.push(response.data);
+        console.log("added a new product! now purchase it!");
+      });
+    }
+  }
 };
 </script>
