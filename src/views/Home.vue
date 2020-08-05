564fc19f45a696066da5d375eeba34348e85b4fc
PeterH <pete08@gmail.com>
2020-08-04 21:15:09 -0500

    Add a destroy method to delete items in the products table using destroyProduct method -> See <script> methods

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="home">
-  <h1>{{ message }}</h1>
+    <h1>{{ message }}</h1>
 
     ---------------------
-    <br>
-    <button v-on:click="indexRecipes()">Get some data...maybe</button>
     <br>
     <button v-on:click="indexProducts()">Get some product data from the api</button>
     <br>
@@ -18,6 +16,7 @@
     <div v-for="product in products">
       <br>
       <br>
+      <p>Id: {{product.id}}</p>
       <p> name: {{ product.name }} </p>
       <p> description: {{ product.description }} </p>
       <p> price: {{ product.price }} </p>
@@ -29,37 +28,29 @@
       <hr>
     </div>
 
+
+    <!-- UPDATE With Modal HERE: -->
+    <!--  -->
     <dialog id="product-details">
       <form method="dialog">
-        <h1>Product Stuff</h1>
-        <p><b>{{currentProduct.name}}</b></p>
-        <p>Description: {{currentProduct.description}}</p>
-        <p>It'll cost ya!: {{currentProduct.price}}</p>
-        <p>Image URL: {{currentProduct.image_url}}</p>
+        <h1>Product Details</h1>
+        <p>Name: <input type="text" v-model="currentProduct.name"></p>
+        <p>Description: <input type="text" v-model="currentProduct.description"></p>
+        <p>what'll it cost: <input type="text" v-model="currentProduct.price"></p>
+        <p>Image URL: <input type="text" v-model="currentProduct.image_url"></p>
+        <button v-on:click="updateProduct(currentProduct)">Update Product!</button>
         <button>Close</button>
+        <button v-on:click="destroyProduct(currentProduct)">Delete Product</button>
       </form>
     </dialog>
-
-<!-- UPDATE With Modal HERE! -->
-<!--  -->
-    <!-- <dialog id="product-update">
-      <form method="dialog">
-        <h1>Update this here product!</h1>
-        <p><b>{{currentProduct.name}}</b></p>
-        <p>Description: <input type="text" v-model="description"></p>
-        <p>It'll cost ya!: {{currentProduct.price}}</p>
-        <p>Image URL: {{currentProduct.image_url}}</p>
-        <button v-on:click="updateProduct"(currentProduct)
-        <button>Close</button>
-      </form>
-    </dialog> -->
-<!--  -->
+    <!-- UPDATE With Modal up there ^ -->
 
   </div>
 </template>
 
-<style>
 
+
+<style>
 </style>
 
 
@@ -82,10 +73,6 @@ export default {
 
   methods: {
 
-    indexRecipes: function() {
-      console.log("HI THERE!! From console");
-    },
-    
     indexProducts: function() {
       console.log("You executed the indexProducts by either refreshign page or clicking  event listener button");
       axios.get('api/products').then(response => {
@@ -93,7 +80,6 @@ export default {
         this.products = response.data;
       });
     },
-
 
 
     createProduct: function() {
@@ -112,10 +98,40 @@ export default {
       });
     },
 
+
     showInfo: function(product) {
       console.log(product);
       this.currentProduct = product;
       document.querySelector('#product-details').showModal();
+    },
+
+    updateProduct: function(product) {
+      console.log(product);
+
+      var params = {
+        name: this.currentProduct.name ,
+        description: this.currentProduct.description ,
+        price: this.currentProduct.price ,
+        image_url: this.currentProduct.image_url ,
+      };
+
+      axios.patch('/api/products/' + this.currentProduct.id, params).then(response => {
+        console.log(response.data);
+        this.currentProduct = response.data;
+      });
+
+    },
+
+    destroyProduct: function(product) {
+      console.log(product);
+      // delete it in the backend (rails)
+      axios.delete('api/products/' + product.id).then(response => {
+        console.log(response.data);
+        // delete in frontend 
+        var index = this.products.indexOf(product);
+        this.products.splice(index, 1);
+        console.log(index);
+      });
     }
 
   }
