52403062f595504f744861a6ffafe757ebe51aba
PeterH <pete08@gmail.com>
2020-08-04 20:07:47 -0500

    Add Show using modals through use of dialog box. See <template>, See within <script>: data function and methods

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home">
-    <h1>{{ message }}</h1>
+  <div class="home">
+  <h1>{{ message }}</h1>
 
     ---------------------
     <br>
@@ -10,23 +10,52 @@
     <br>
     <button v-on:click="createProduct()">Get it! Make IT!</button>
     <br>
-    <p>name: <input type="text" v-model="name"></p>
+    <!-- <p>name: <input type="text" v-model="name"></p>
     <p>description: <input type="text" v-model="description"></p>
     <p>price: <input type="text" v-model="price"></p>
-    <p>image url: <input type="text" v-model="image_url"></p>
-    <br>
-    <br>
+    <p>image url: <input type="text" v-model="image_url"></p> -->
     <hr>
     <div v-for="product in products">
+      <br>
+      <br>
       <p> name: {{ product.name }} </p>
       <p> description: {{ product.description }} </p>
       <p> price: {{ product.price }} </p>
       <p> img: {{ product.image_url }} </p>
-      <img v-bind:src="product.image_url">
+      <button v-on:click="showInfo(product)">Show more info</button>
+      <p><img v-bind:src="product.image_url"></p>
+      <br>
+      <br>
       <hr>
     </div>
-    <br>
-  </div>
+
+    <dialog id="product-details">
+      <form method="dialog">
+        <h1>Product Stuff</h1>
+        <p><b>{{currentProduct.name}}</b></p>
+        <p>Description: {{currentProduct.description}}</p>
+        <p>It'll cost ya!: {{currentProduct.price}}</p>
+        <p>Image URL: {{currentProduct.image_url}}</p>
+        <button>Close</button>
+      </form>
+    </dialog>
+
+<!-- UPDATE With Modal HERE! -->
+<!--  -->
+    <!-- <dialog id="product-update">
+      <form method="dialog">
+        <h1>Update this here product!</h1>
+        <p><b>{{currentProduct.name}}</b></p>
+        <p>Description: <input type="text" v-model="description"></p>
+        <p>It'll cost ya!: {{currentProduct.price}}</p>
+        <p>Image URL: {{currentProduct.image_url}}</p>
+        <button v-on:click="updateProduct"(currentProduct)
+        <button>Close</button>
+      </form>
+    </dialog> -->
+<!--  -->
+
+  </div>
 </template>
 
 <style>
@@ -43,6 +72,7 @@ export default {
     return {
       message: "Welcome to Vue.js! HEllo@",
       products: [],
+      currentProduct: {}
     };
   },
 
@@ -80,7 +110,14 @@ export default {
         this.products.push(response.data);
         console.log("added a new product! now purchase it!");
       });
+    },
+
+    showInfo: function(product) {
+      console.log(product);
+      this.currentProduct = product;
+      document.querySelector('#product-details').showModal();
     }
+
   }
 };
 </script>
